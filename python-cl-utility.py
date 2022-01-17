6b4f9899150e05d7d36b1c9b9c14eb1c816e029e
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2022-01-17 20:22:58 +0530

    learned
    17/01/2022

---
+++ python-cl-utility.py
@@ -0,0 +1,31 @@
+import argparse
+from sqlite3 import paramstyle
+import sys
+
+def calc(args):
+    if args.o == 'add':
+        return args.x + args.y
+    elif args.o == 'mul':
+        return args.x * args.y
+    elif args.o == 'sub':
+        return args.x - args.y
+    elif args.o == 'div':
+        return args.x / args.y
+
+    else:
+        return "Something went wrong"
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x', type=float, default=1.0, 
+    help="Enter first number. This is a utility for calculation. Please Contact Mayank The developer")
+
+    parser.add_argument('--y', type=float, default=3.0,
+    help="Enter second number. This is a utility for calculation. Please contact mayank the developer")
+
+    parser.add_argument('--o', type=str, default="add",
+    help="This is a utility for calculation. Please contact mayank the developer")
+
+    args = parser.parse_args()
+    sys.stdout.write(str(calc(args)))
