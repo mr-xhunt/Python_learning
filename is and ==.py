39c1b9877e7cd4712920e154661f82eae43497cb
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2022-01-17 19:56:08 +0530

    learned
    17/01/2022

---
+++ is and ==.py
@@ -0,0 +1,9 @@
+# is  : reference equality : two references refer to the same object
+# ==  : value equality     : two object having same values
+
+
+a = [6, 4, "34"]
+b = [6, 4, "34"]
+
+print(a == b)
+print(a is b)
